af5fa83b9a4f244cb4f38c9309303c765b5e1701
hojung7 <wjdghwjd99@naver.com>
2024-10-08 17:43:40 +0900

    20241008_게시글 좋아요, 조회수 증가, 인터셉터

--- boardProject/src/main/java/edu/kh/project/board/controller/EditBoardController.java
+++ boardProject/src/main/java/edu/kh/project/board/controller/EditBoardController.java
@@ -87,7 +87,7 @@ public class EditBoardController {
 			path = "insert";
 			message = "게시글 작성 실패";
 		}else {
-			// path = "/board/" + boardCode + "/" + boardNo; // 상세 조회 주소
+			 path = "/board/" + boardCode + "/" + boardNo; // 상세 조회 주소
 			
 			path="/board/" + boardCode; // 목록 조회 주소(임시)
 			message = "게시글이 작성 되었습니다";
