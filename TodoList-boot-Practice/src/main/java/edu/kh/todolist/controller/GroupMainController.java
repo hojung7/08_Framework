17a11b85cdb70277c49f9990693783541c805219
hojung7 <wjdghwjd99@naver.com>
2024-10-10 17:49:26 +0900

    20241010_게시글 삭제, 수정

--- TodoList-boot-Practice/src/main/java/edu/kh/todolist/controller/GroupMainController.java
+++ TodoList-boot-Practice/src/main/java/edu/kh/todolist/controller/GroupMainController.java
@@ -2,14 +2,21 @@ package edu.kh.todolist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kh.todolist.service.GroupMainService;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Controller
 public class GroupMainController {
  
 	private GroupMainService service;
+	
+	@RequestMapping("/")
+	public String groupMain() {
+		return "groupMain/groupMain";
+	}
 	
 }
