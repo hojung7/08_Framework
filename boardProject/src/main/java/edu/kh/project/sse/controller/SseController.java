8e975114cb4608407c5ff4e4202101347007dd55
hojung7 <wjdghwjd99@naver.com>
2024-11-30 21:08:34 +0900

    20241130_알림 복습

--- boardProject/src/main/java/edu/kh/project/sse/controller/SseController.java
+++ boardProject/src/main/java/edu/kh/project/sse/controller/SseController.java
@@ -23,137 +23,124 @@ import lombok.extern.slf4j.Slf4j;
 @RestController // @controller + @ResponseBody
 @Slf4j
 public class SseController {
-	
+
 	@Autowired
 	private SseService service;
 
-	//SseEmitter : 서버로 부터 메시지를 전달 받을
-	// 										클라이언트 정보를 저장한 객체 == 연결된 클라이언트
-	
+	// SseEmitter : 서버로 부터 메시지를 전달 받을
+	// 클라이언트 정보를 저장한 객체 == 연결된 클라이언트
+
 	// ConcurrentHashMap : 멀티스레드 환경에서 동기화를 보장하는 Map
 	// -> 한번에 많은 요청이 있어도 차례대로 처리
-	private final Map<String, SseEmitter> emitters = 
-			new ConcurrentHashMap<>(); // == 연결된 클라이언트 대기 명단
-	
- /** 클라이언트 연결 요청 처리 */
+	private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>(); // == 연결된 클라이언트 대기 명단
+
+	/** 클라이언트 연결 요청 처리 */
 	@GetMapping("sse/connect")
-	public SseEmitter sseConnect(
-			@SessionAttribute("loginMember") Member loginMember) {
-		
-		//Map에 저장될 key 값으로 회원 번호 얻어오기
+	public SseEmitter sseConnect(@SessionAttribute("loginMember") Member loginMember) {
+
+		// Map에 저장될 key 값으로 회원 번호 얻어오기
 		String clientId = loginMember.getMemberNo() + "";
-		
+
 		// SseEmiter 객체 생성
 		// -> 연결 대기 시간 10분 설정
-		SseEmitter emitter = new SseEmitter(10*60*1000L);
-		
+		SseEmitter emitter = new SseEmitter(10 * 60 * 1000L);
+
 		// 클라이언트 정보를 Map에 추가
 		emitters.put(clientId, emitter);
-		
+
 		// 클라이언트 연결 종료 시 Map에서 제거
 		emitter.onCompletion(() -> emitters.remove(clientId));
-		
+
 		// 클라이언트 타임 아웃 시 Map에서 제거
 		emitter.onTimeout(() -> emitters.remove(clientId));
-		
+
 		return emitter;
 
-		
 	}
-	
-	/**알림 메시지 전송*/
+
+	/** 알림 메시지 전송 */
 	@PostMapping("sse/send")
-	public void sendNotification(
-			@RequestBody Notification notification,
-			@SessionAttribute("loginMember") Member loginMember
-			) {
-		
+	public void sendNotification(@RequestBody Notification notification,
+			@SessionAttribute("loginMember") Member loginMember) {
+
 		// 알림 보낸 사람(현재 로그인한 회원) 번호 추가
 		notification.setSendMemberNo(loginMember.getMemberNo());
-		
+
 		// 전달 받은 알림 데이터를 DB에 저장하고
 		// 알림 받을 회원의 번호
 		// + 해당 회원이 읽지 않은 알림 개수 반환 받는 서비스 호출
-		Map<String, Object> map
-		= service.insertNotification(notification);
-		
+		Map<String, Object> map = service.insertNotification(notification);
+
 		// 알림을 받을 클라이언트 id(수정 예정)
 
-	String clientId = map.get("receiveMemberNo").toString();
-		
+		String clientId = map.get("receiveMemberNo").toString();
+
 		// 연결된 클라이언트 대기 명단(emitters)에서
 		// clientId가 일치하는 클라이언트 찾기
 		SseEmitter emitter = emitters.get(clientId);
-		
+
 		// clientId가 일치하는 클라이언트가 있을 경우
-		if(emitter != null) {
-			
+		if (emitter != null) {
+
 			try {
 				emitter.send(map);
-			}catch(Exception e) {
-			emitters.remove(clientId);
-		}
+			} catch (Exception e) {
+				emitters.remove(clientId);
+			}
 		}
 	}
-	
+
 //	sendNotification("boardLike", "/bordLike/1/2024, 2024, "좋아요 눌림")
-	
-	
-	//------------------------------------------------------------
-	/**로그인한 회원의 알림 목록 조회
+//	sendNotification("boardLike", "/bordLike/2/2024, 2024, "좋아요 눌림")
+
+	// ------------------------------------------------------------
+	/**
+	 * 로그인한 회원의 알림 목록 조회
+	 * 
 	 * @param loginMember
 	 * @return
 	 */
 	@GetMapping("notification")
-	public List<Notification> selectNotificationList(
-		@SessionAttribute("loginMember") Member loginMember
-			){
-			int memberNo = loginMember.getMemberNo();
-			return service.selectNotificationList(memberNo);
-}
-	
-	/** 현재 로그인한 회원의 알림 중 
-	 * 읽지 않은 알림 개수를 조회
-	 * ("NOTOFICATION".NOTIFICATION_CHECK = 'N')
+	public List<Notification> selectNotificationList(@SessionAttribute("loginMember") Member loginMember) {
+		int memberNo = loginMember.getMemberNo();
+		return service.selectNotificationList(memberNo);
+	}
+
+	/**
+	 * 현재 로그인한 회원의 알림 중 읽지 않은 알림 개수를 조회 ("NOTOFICATION".NOTIFICATION_CHECK = 'N')
+	 * 
 	 * @return
 	 */
 	@GetMapping("notification/notReadCheck")
-	public int notReadCheck(
-			@SessionAttribute("loginMember") Member loginMember) {
-		
-		
-		 return service.notReadCheck(loginMember.getMemberNo());
+	public int notReadCheck(@SessionAttribute("loginMember") Member loginMember) {
 
-		
-		
-		
+		return service.notReadCheck(loginMember.getMemberNo());
+
 	}
-	
-	/** 알림 삭제
+
+	/**
+	 * 알림 삭제
+	 * 
 	 * @param notificationNo
 	 * @return
 	 */
 	@DeleteMapping("notification")
-	public void deleteNotification(
-			@RequestBody int notificationNo) {
-		
+	public void deleteNotification(@RequestBody int notificationNo) {
+
 		service.deleteNotification(notificationNo);
-		
-		
+
 	}
-	
-	/** 알림 읽음 여부 변경(N->Y)
+
+	/**
+	 * 알림 읽음 여부 변경(N->Y)
+	 * 
 	 * @param notificationNo
 	 */
 	@PutMapping("notification")
-	public void updateNotification(
-			@RequestBody int notificationNo) {
-		
+	public void updateNotification(@RequestBody int notificationNo) {
+
 		service.updateNotification(notificationNo);
-		
+
 	}
-	
-	
-	
-	
+
 }
